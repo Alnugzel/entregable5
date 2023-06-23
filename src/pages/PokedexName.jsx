7f9feda39af0955be461eeeb5d47ebd08492ac60
Alnugzel <angelfiguera0889@gmail.com>
2023-06-23 16:07:29 -0400

    continue

--- src/pages/PokedexName.jsx
+++ src/pages/PokedexName.jsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 import useFetch from "../hook/useFetch";
 import { useEffect } from "react";
+import "../components/Pokedex/styles/PokedexName.css";
 
 const PokedexName = () => {
   const { name } = useParams();
@@ -14,19 +15,34 @@ const PokedexName = () => {
   }, [name]);
 
   return (
-    <div>
+    <article className="pokedexName">
       {hasError ? (
         <h1>This pokemon is not exist</h1>
       ) : (
         <>
-          <img
-            src={pokemon?.sprites.other["official-artwork"].front_default}
-            alt=""
-          />
-          <h2>{pokemon?.name}</h2>
+          <header className="pokedexName_header">
+            <img
+              className="pokedexName_img"
+              src={pokemon?.sprites.other["official-artwork"].front_default}
+              alt=""
+            />
+          </header>
+
+          <section className="pokedexName__body">
+            <h2>{pokemon?.name}</h2>
+
+            <h3 className="pokedexName__name">{pokemon?.name}</h3>
+            <ul className="pokedexName__types">
+              {pokemon?.types.map((typeInfo) => (
+                <li className="pokedexName__types-item" key={typeInfo.type.url}>
+                  {typeInfo.type.name}
+                </li>
+              ))}
+            </ul>
+          </section>
         </>
       )}
-    </div>
+    </article>
   );
 };
 
