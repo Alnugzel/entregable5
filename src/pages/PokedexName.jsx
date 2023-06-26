eb64a9769e34bbdc5ad872ba4831e62f5b5cc7c3
Alnugzel <angelfiguera0889@gmail.com>
2023-06-26 15:55:29 -0400

    lastcard

--- src/pages/PokedexName.jsx
+++ src/pages/PokedexName.jsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom";
 import useFetch from "../hook/useFetch";
 import { useEffect } from "react";
 import "../components/Pokedex/styles/PokedexName.css";
+import HeaderApi from "../utils/HeaderApi";
 
 const PokedexName = () => {
   const { name } = useParams();
@@ -14,35 +15,79 @@ const PokedexName = () => {
     getPokemon();
   }, [name]);
 
+  console.log(pokemon);
+
   return (
-    <article className="pokedexName">
-      {hasError ? (
-        <h1>This pokemon is not exist</h1>
-      ) : (
-        <>
-          <header className="pokedexName_header">
-            <img
-              className="pokedexName_img"
-              src={pokemon?.sprites.other["official-artwork"].front_default}
-              alt=""
-            />
-          </header>
+    <>
+      <HeaderApi />
+      <div className="pokedexName__container">
+        <article className="pokedexName">
+          {hasError ? (
+            <h1>This pokemon is not exist</h1>
+          ) : (
+            <>
+              <header className="pokedexName_header">
+                <img
+                  className="pokedexName__img"
+                  src={pokemon?.sprites.other["official-artwork"].front_default}
+                  alt=""
+                />
+              </header>
+              <div className="pokedexName__id">
+                <h1> #{pokemon?.id}</h1>
+              </div>
 
-          <section className="pokedexName__body">
-            <h2>{pokemon?.name}</h2>
+              <section className="pokedexName__body">
+                <h2 className="pokedexName__name">{pokemon?.name}</h2>
+                <div className="pokedexName__sizes">
+                  <p>
+                    <span>weight</span> <span>{pokemon?.weight}</span>
+                  </p>
+                  <p>
+                    <span>height</span> <span>{pokemon?.height}</span>
+                  </p>
+                </div>
+                <ul className="pokedexName__types">
+                  {pokemon?.types.map((typeInfo) => (
+                    <li
+                      className="pokedexName__types-item"
+                      key={typeInfo.type.url}
+                    >
+                      {typeInfo.type.name}
+                    </li>
+                  ))}
+                </ul>
+              </section>
 
-            <h3 className="pokedexName__name">{pokemon?.name}</h3>
-            <ul className="pokedexName__types">
-              {pokemon?.types.map((typeInfo) => (
-                <li className="pokedexName__types-item" key={typeInfo.type.url}>
-                  {typeInfo.type.name}
-                </li>
-              ))}
-            </ul>
-          </section>
-        </>
-      )}
-    </article>
+              <section className="pokedexName__stats-img">
+                <div>
+                  <img src="/stats.png" alt="stats" />
+                </div>
+
+                <ul>
+                  <li>
+                    <span>{pokemon?.stats[0].stat.name}</span>{" "}
+                    <span>{pokemon?.stats[0].base_stat}</span>
+                  </li>
+                  <li>
+                    <span>{pokemon?.stats[1].stat.name}</span>{" "}
+                    <span>{pokemon?.stats[1].base_stat}</span>
+                  </li>
+                  <li>
+                    <span>{pokemon?.stats[2].stat.name}</span>{" "}
+                    <span>{pokemon?.stats[2].base_stat}</span>
+                  </li>
+                  <li>
+                    <span>{pokemon?.stats[5].stat.name}</span>{" "}
+                    <span>{pokemon?.stats[5].base_stat}</span>
+                  </li>
+                </ul>
+              </section>
+            </>
+          )}
+        </article>
+      </div>
+    </>
   );
 };
 
